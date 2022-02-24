56613ef8ca679b4c92aa30fd4c7f704103045451
Vlad Somai <58813056+vladsomai@users.noreply.github.com>
2022-02-24 10:36:19 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace BoardVersioning_UI
+{
+    partial class BoardVersioning
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BoardVersioning));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Parse_Button = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaShell;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaShell;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Label1
+            // 
+            resources.ApplyResources(this.Label1, "Label1");
+            this.Label1.Name = "Label1";
+            // 
+            // Label2
+            // 
+            resources.ApplyResources(this.Label2, "Label2");
+            this.Label2.Name = "Label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Parse_Button
+            // 
+            this.Parse_Button.BackColor = System.Drawing.Color.SeaShell;
+            resources.ApplyResources(this.Parse_Button, "Parse_Button");
+            this.Parse_Button.Name = "Parse_Button";
+            this.Parse_Button.UseVisualStyleBackColor = false;
+            this.Parse_Button.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.richTextBox1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SeaShell;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // BoardVersioning
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Parse_Button);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "BoardVersioning";
+            this.Load += new System.EventHandler(this.BoardVersioning_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Parse_Button;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace BoardVersioning_UI
+{
+    public partial class BoardVersioning : Form
+    {
+        public BoardVersioning()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BoardVersioning_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //browse button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog raw = new OpenFileDialog();
+            if (raw.ShowDialog() == DialogResult.OK)
+            {
+                string rawFile = raw.FileName;
+                textBox1.Text = rawFile;
+            }
+            raw.Dispose();
+        }
+
+        //browse button
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            using (OpenFileDialog raw = new OpenFileDialog())
+            {
+                if (raw.ShowDialog() == DialogResult.OK)
+                {
+                    string rawFile = raw.FileName;
+                    textBox2.Text = rawFile;
+                }
+            }
+        }
+        //browse button
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+         
+            using (OpenFileDialog board = new OpenFileDialog())
+            {
+                if (board.ShowDialog() == DialogResult.OK)
+                {
+                    string boardpath = board.FileName;
+                    textBox3.Text += boardpath;
+                }
+            }
+        }
+
+        //text box for raw bom path
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //enter in log the bom path
+            richTextBox1.AppendText("The raw BOM is: \n");
+            richTextBox1.AppendText(textBox1.Text+"\n");
+        }
+
+        //loggging window
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //make the richtextbox autoscroll to last item
+            richTextBox1.Focus();    
+        }
+
+        //text box for variant bom path
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("The variant BOM is: \n");
+            richTextBox1.AppendText(textBox2.Text + "\n");
+        }
+
+        //parse button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string rawPath = textBox1.Text;
+            string variantPath = textBox2.Text;
+            string boardPath = textBox3.Text;
+            string versionName = textBox4.Text;
+
+            //string rawPath     = @"C:\Users\uidn4945\Downloads\20053011_AD_AsyPartListProp_Max.csv";
+            //string variantPath = @"C:\Users\uidn4945\Downloads\20060556_AA_AsyPartListProp_Var.csv";
+            //string boardPath   = @"C:\Users\uidn4945\Downloads\board";
+            //string versionName = @"V02";
+
+            if (!File.Exists(rawPath))
+            {
+                richTextBox1.AppendText("Raw BOM not found!\n");
+                return;
+            }
+            if (!File.Exists(variantPath))
+            {
+                richTextBox1.AppendText("Variant BOM not found!\n");
+                return;
+            }
+            if (!File.Exists(boardPath))
+            {
+                richTextBox1.AppendText("\"board\" file not found!\n");
+                return;
+            }
+
+            if(versionName.Length == 0)
+            {
+                richTextBox1.AppendText("Please enter a version name!\n");
+                return;
+            }
+
+            //READ THE BOMS -> ADD THEM INTO AN ARRAY
+            string[] rawBom = System.IO.File.ReadAllLines(rawPath);
+            string[] variantBom = System.IO.File.ReadAllLines(variantPath);
+            List<string> rawDesignators = new List<string>();
+            List<string> variantDesignators = new List<string>();
+
+            string[] splitLine;
+
+            //Add only lines that contain A2C into a list
+            foreach (string line in rawBom)
+            {
+                if (line[0] == '#')
+                {
+
+                }
+                else if (line.Contains("A2C") || line.Contains("A3C"))
+                {
+                    splitLine = line.Split(';');
+                    rawDesignators.Add(splitLine[0]);
+                }
+            }
+
+            foreach (string line in variantBom)
+            {
+                if (line[0] == '#')
+                {
+
+                }
+                else if (line.Contains("A2C") || line.Contains("A3C"))
+                {
+                    splitLine = line.Split(';');
+                    variantDesignators.Add(splitLine[0]);
+                }
+            }
+
+            List<string> missingInVariant = new List<string>();
+            foreach (string designator in rawDesignators)
+            {
+                if (!variantDesignators.Contains(designator))
+                    missingInVariant.Add(designator);
+            }
+
+            //Parse the board file
+            string[] board = File.ReadAllLines(boardPath);
+            string modBoardPath = desktopPath + @"\board";
+            StreamWriter modBoard = new StreamWriter(modBoardPath);//create the board to be modified
+
+            bool nodesFound = false;//in case word "NODES" is found continue writing the BOM file.
+            foreach (string boardLine in board)
+            {
+                if (boardLine.Contains("HEADING"))
+                    modBoard.Write($"VERSIONS\n  {versionName};\n\n");
+
+                if (boardLine.Contains("NODES") || boardLine.Contains("PIN_MAP") || boardLine.Contains("CONNECTIONS"))
+                    nodesFound = true;
+
+                if (nodesFound)
+                {
+                    modBoard.WriteLine(boardLine);
+                }
+                else
+                {
+                    splitLine = boardLine.Split(' ');
+                    try
+                    {
+                        if(boardLine.Contains("VERSION"))
+                        {
+                            modBoard.WriteLine(boardLine);
+                            continue;
+                        }
+                        foreach (string designator in missingInVariant)
+                        {
+                            if (splitLine[2] == designator)
+                            {
+                                if (splitLine[2][0] == 'X')//connectors done have "PN" keyword, we use "NT" instead
+                                {
+                                    string tempBoardLinePar1 = boardLine.Substring(0, boardLine.IndexOf("NT"));
+                                    string tempBoardLinePar2 = boardLine.Substring(boardLine.IndexOf("NT"), boardLine.IndexOf(";") - boardLine.IndexOf("NT"));
+                                    modBoard.WriteLine(tempBoardLinePar1 + " NP " + tempBoardLinePar2 + $"   VERSION {versionName};");
+                                }
+                                else // all other components contain PN keyword
+                                {
+                                    string tempBoardLinePar1 = boardLine.Substring(0, boardLine.IndexOf("PN"));
+                                    string tempBoardLinePar2 = boardLine.Substring(boardLine.IndexOf("PN"), boardLine.IndexOf(";") - boardLine.IndexOf("PN"));
+                                    modBoard.WriteLine(tempBoardLinePar1 + " NP " + tempBoardLinePar2 + $"   VERSION {versionName};");
+                                }
+                            }
+                        }
+                        modBoard.WriteLine(boardLine);
+                    }
+                    catch (Exception)
+                    {
+                        modBoard.WriteLine(boardLine);
+                    }
+                }
+            }
+            modBoard.Close();
+            richTextBox1.AppendText("\nParsing done, please check file \"modBoard\" on your Desktop!\nUse command \"check board \"modBoard\";list\" to identify any errors that may occur!");
+        }
+    }
+}
